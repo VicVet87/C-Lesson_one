67f3c816fba9a424a7ce32bf5dd7725c48284051
Vettsel <vic87@gmail.com>
2022-10-15 09:01:09 +0500

    добавил Ex004 и удалил лишнее

---
+++ Ex004/Program.cs
@@ -0,0 +1,22 @@
+﻿string message = "Вам письмо!";
+string password = "12345@";
+int i = 0;
+while(i < 3)
+{
+    i++;
+  Console.WriteLine("Введте пароль");
+  if(Console.ReadLine()!=password)
+  {
+     Console.WriteLine("Пароль не верный");
+     continue;
+  }
+  else
+  {
+    Console.WriteLine(message);
+    i=0;
+    break;
+  }  
+}
+if (i==3)
+    Console.WriteLine("Доступ запрещен!");
+
