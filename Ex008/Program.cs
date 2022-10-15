c72f798f770ab46a9e430ad68a8611bec497ae2d
Vettsel <vic87@gmail.com>
2022-10-15 11:34:26 +0500

    добавил Ex008

---
+++ Ex008/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+int a;
+int i = 2; 
+string result = "";
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+while (i <= a)
+{
+    if(i%2 == 0)
+    result += i + ",";
+    i++;
+}
+Console.WriteLine(result.Substring(0, result.Length - 1));
