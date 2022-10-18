fa77e585807d95ab96dd0188b01e8a6bf005fd74
Vettsel <vic87@gmail.com>
2022-10-18 23:39:26 +0500

    добавил запрос старого пароля и мелкие фиксы

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -7,30 +7,53 @@ SetPassword – Установить пароль
 Exit – выход
 WriteName – вывести имя после ввода пароля
 */
-string name = "";
+string? name = null;
 string? password = null;
 bool exit = false;
-List<String> comands = new List<string> {"Help", "SetName", "SetPassword", "WriteName", "Exit"};
-while (true)
+Dictionary<string, string> comands = new Dictionary<string, string>(){
+    {"Help",        "вывоодит все доступные команды"},
+    {"SetName",     "позволяет задать имя пользователя"},
+    {"SetPassword", "позволяет задать пароль пользователя(если установлен необходимо ввести старый пароль)"},
+    {"WriteName",   "вывод имени пользователя(если установлин пароль необходимо ввести его)"},
+    {"Exit",        "выход из программы"}  
+                                                                     };
+while (!exit)
 {    
     switch ((Console.ReadLine()??"").ToLower())
     {
         case "help": 
-                    foreach (string com in comands)
+                    foreach (KeyValuePair<string, string> com in comands)
                     {
-                        Console.WriteLine(com);
+                        Console.WriteLine("{0} - {1}", com.Key, com.Value);
                     }
                     break;
         case "setname":
                        Console.WriteLine("Введите имя");
-                       name = Console.ReadLine() ?? "";
+                       name = Console.ReadLine();
                        break;
         case "setpassword":
+                        if (password is not null)
+                        {
+                            Console.WriteLine("Введите старый пароль");
+                            if(password == Console.ReadLine())
+                            {
+                                Console.WriteLine("Введите новый пароль");
+                                password = Console.ReadLine();                                
+                            }                                
+                            else                            
+                                Console.WriteLine("Пароль не верный");
+                            break;
+                        } 
                        Console.WriteLine("Введите пароль");
                        password = Console.ReadLine();
                        break;
         case "writename":
-                        if (password is not null)
+                        if (name is null)
+                        {
+                            Console.WriteLine("Имя не задано");
+                            break;
+                        }
+                        else if (password is not null)
                         {
                             Console.WriteLine("Введите пароль");
                             if(password == (Console.ReadLine() ?? ""))
@@ -39,8 +62,12 @@ while (true)
                                 Console.WriteLine("Пароль не верный");
                             break;
                         } 
-                        Console.WriteLine(name); break;
-        case "exit": exit = true; break;    
-    } 
-    if(exit) break;
+                        else
+                        {
+                            Console.WriteLine(name);
+                            break;
+                        }
+        case "exit": exit = true;
+                     break;    
+    }     
 }
