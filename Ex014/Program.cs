3d022fae1bb3fbef3a837100be9c71e67558a8d9
Vettsel <vic87@gmail.com>
2022-10-25 00:23:13 +0500

    мелкие fix-ы

--- Ex014/Program.cs
+++ Ex014/Program.cs
@@ -1,14 +1,15 @@
 ﻿/*Задача 23
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
 string error = string.Empty;
-int tempNumber;
-string result = string.Empty;
+uint tempNumber;
+string result;
 while (error != "exit")
 {
-    tempNumber = GetIntNumberRequest("Введите число(для выхода введите exit)", out error);
+    tempNumber = GetIntNumberRequest("Введите положительное число(для выхода введите exit)", out error);
     
     if (error == string.Empty)
     {
+        result = string.Empty;
         for (int i = 1; i < tempNumber; i++)
         {
             result += Math.Pow(i,3) + ", ";
@@ -21,9 +22,9 @@ while (error != "exit")
     }
 }
 
-int GetIntNumberRequest(string message, out string error)
+uint GetIntNumberRequest(string message, out string error)
  {
-    int result = 0;
+    uint result = 0;
     string? temstring;
     error = string.Empty;
     try
@@ -40,7 +41,7 @@ int GetIntNumberRequest(string message, out string error)
         }
         else
         {
-            result = Convert.ToInt32(temstring);
+            result = Convert.ToUInt32(temstring);
         }       
     }
     catch (System.Exception ex)
